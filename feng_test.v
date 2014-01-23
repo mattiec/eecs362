154e4e9d820a67df4380bc4ec4744a4357453de9
Chiyuan Feng <cff778@batgirl.ece.northwestern.edu>
2014-01-22 21:27:26 -0600

    testing

--- feng_test.v
+++ feng_test.v
@@ -1,1 +1,16 @@
-This is test
+module FA_Seq (A,B,Cin,Sum,Cout);
+	input A,B,Cin
+	output Sum, Cout;
+	reg Sum, Cout;
+	reg T1, T2, T3;
+
+	always
+	    @ (A or B or Cin) begin 
+		Sum =(A^B)^Cin;
+		T1=A&Cin;
+		T2=B&Cin;
+		T3=A&B;
+		Cout=(T1|T2)|T3;
+		end
+endmodule
+
