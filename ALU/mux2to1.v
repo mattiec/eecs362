30fe2fb120703408fb5e1e8722ff488ba3101d0a
Neeraj Chandra <soccernee@yahoo.com>
2014-01-27 23:09:59 -0600

    subtraction functionality in ALU

---
+++ ALU/mux2to1.v
@@ -0,0 +1,11 @@
+
+module mux2to1 (a,b,sel,out);
+        parameter width = 32;
+        input [0:width-1] a,b;
+        input  sel;
+
+        output [0:width-1] out;
+
+        assign out = (sel == 1'b0) ? a:
+                (sel == 1'b1) ? b;
+endmodule
--- ALU/tests/adder_test.v
+++ ALU/tests/adder_test.v
@@ -4,7 +4,7 @@ module adder_test;
       reg CIN;
       wire [31:0] SUM;
       wire COUT;
-
+     
       fa_32bit adder(A,B,CIN,SUM,COUT);
 
     initial begin
@@ -12,10 +12,10 @@ module adder_test;
           #0 A = 32'h0000; B = 32'h0000; CIN = 1'b0;
           #1 A = 32'h0000; B = 32'h0000; CIN = 1'b1;
           #1 A = 32'h0001; B = 32'h0000; CIN = 1'b0;
-          #1 A = 32'b0; B = 32'b1; CIN = 1'b1;
-          #1 A = 32'b1; B = 32'b0; CIN = 1'b0;
-          #1 A = 32'b1; B = 32'b0; CIN = 1'b1;
-          #1 A = 32'b1; B = 32'b1; CIN = 1'b0;
-          #1 A = 32'b1; B = 32'b1; CIN = 1'b1;
+          #1 A = 32'h0001; B = 32'h0000; CIN = 1'b1;
+          #1 A = 32'h000B; B = 32'h000A; CIN = 1'b0;
+          #1 A = 32'h000B; B = 32'h000A; CIN = 1'b1;
+          #1 A = 32'hFFFF; B = 32'hFFFF; CIN = 1'b0;
+          #1 A = 32'hFFFF; B = 32'hFFFF; CIN = 1'b1;
     end
 endmodule // testbench
