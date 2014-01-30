3219aa2fa75ab15bb2bac31002e53c9e638dbaa2
Shantaviae Wynn <shantaviaewynn2014@u.northwestern.edu>
2014-01-30 16:07:45 -0600

    Complete MULT

--- ALU/tests/mult_test.v
+++ ALU/tests/mult_test.v
@@ -1,17 +1,24 @@
 module mult_test;
-      reg [7:0] A;
-      reg [7:0] B;
-      wire [7:0] PROD;
+      reg [31:0] A;
+      reg [31:0] B;
+      wire [31:0] PROD;
      
-      mult8bit multiplier(A,B,PROD);
+      mult32bit multiplier(A,B,PROD);
 
     initial begin
           $monitor("A=%b B=%b PROD=%b", A, B, PROD);
-          #0 A = 8'b00000000; B = 4'b0000;
-          #1 A = 8'b00100000; B = 4'b0001;
-          #1 A = 8'b11110001; B = 4'b0111;
-          #1 A = 8'b11111111; B = 4'b0011;
-          #1 A = 8'b01101011; B = 4'b0100;
+          #0 A = 32'h0000; B = 32'h0000;
+          #1 A = 32'h0000; B = 32'h0001;
+          #1 A = 32'h0001; B = 32'h0001;
+          #1 A = 32'h0001; B = 32'h0000;		  
+          #1 A = 32'h0001; B = 32'h0111;
+          #1 A = 32'h1111; B = 32'h0F11;
+          #1 A = 32'h0110; B = 32'h010A;
+		  #1 A = 32'hAA1F; B = 32'h0F11;
+          #1 A = 32'hABCD; B = 32'h010A;
+		  #1 A = 32'hFFFF; B = 32'h0001;
+		  #1 A = 32'hFFFF; B = 32'h0000;
+          #1 A = 32'hFFFF; B = 32'h010A;
     end
 endmodule // testbench
  
