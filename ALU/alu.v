ea9025a8a50e4704e9bf482287c4e53a819d43e5
Neeraj Chandra <soccernee@yahoo.com>
2014-02-11 11:36:19 -0600

    working zero signal from ALU

--- ALU/alu.v
+++ ALU/alu.v
@@ -1,10 +1,11 @@
 
-module alu (a, b, ALUCtrl, out);
+module alu (a, b, ALUCtrl, out, zero_signal);
         parameter width = 32;
 
         input [0:width-1] a, b;
 	input [0:3] ALUCtrl;
         output [0:width-1] out;
+	output zero_signal;
 
 	wire [0:width-1] adder_out, and_out, mult_out, or_out, shift_out, xor_out;
 	wire [0:width-1] tmp0, tmp1;
@@ -24,6 +25,8 @@ module alu (a, b, ALUCtrl, out);
 	shift shift1(a,b,ALUCtrl[1:2],shift_out);
 
 	mux8to1 out_mux(and_out, or_out, xor_out, mult_out, shift_out, adder_out, tmp0, tmp1, ALUCtrl[1:3],out);
+
+	assign zero_signal = ~(out[0] | out[1] | out[2] | out[3] | out[4] | out[5] | out[6] | out[7] | out[8] | out[9] | out[10] | out[11] | out[12] | out[13] | out[14] | out[15] | out[16] | out[17] | out[18] | out[19] | out[20] | out[21] | out[22] | out[23] | out[24] | out[25] | out[26] | out[27] | out[28] | out[29] | out[30] | out[31]);
 
 
 endmodule
--- ALU/tests/alu_test.v
+++ ALU/tests/alu_test.v
@@ -3,12 +3,14 @@ module alu_test;
       reg [0:31] B;
       reg [0:3] CTRL;
       output [0:31] OUT;
+      output ZERO_SIGNAL;
 
-      alu alu1(A,B,CTRL,OUT);
+      alu alu1(A,B,CTRL,OUT, ZERO_SIGNAL);
 
     initial begin
-          $monitor("A=%b B=%b CTRL=%b OUT=%b", A, B, CTRL, OUT);
+          $monitor("A=%b B=%b CTRL=%b OUT=%b ZERO_SIGNAL=%b", A, B, CTRL, OUT, ZERO_SIGNAL);
           #0 A = 32'hF054; B = 32'h005F; CTRL = 4'b0000;
+	  #1 A = 32'hF000; B = 32'h000F; CTRL = 4'b0000;
           #1 A = 32'hFF40; B = 32'h00FF; CTRL = 4'b0001;
           #1 A = 32'hFF40; B = 32'h03FF; CTRL = 4'b0010;
           #1 A = 32'h0004; B = 32'h0008; CTRL = 4'b0011;
