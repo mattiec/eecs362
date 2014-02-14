16913ce18e0ff98277bf48e7c49a0eb37b086012
Matthew Shing-Tai Chan <msc494@selina.ece.northwestern.edu>
2014-02-14 16:12:49 -0600

    extender for both unsigned and signed

--- 16bitTo32bitExtender.v
+++ 16bitTo32bitExtender.v
@@ -1,7 +1,29 @@
-module extender (
- input signed [15:0] a,
- output signed [31:0] out);
+//module extender (
+// input signed [15:0] a,
+// output signed [31:0] out);
  
- assign out = a; // sign-extends
+// assign out = a; // sign-extends
  
- endmodule	
+// endmodule
+
+
+
+module extender(a,sign,out);
+	input [15:0] a;
+	input sign; //0 means unsigned, 1 means signed
+	output reg [31:0] out;
+
+	//begin
+	always @* begin	
+		if(sign == 0) begin //unsigned
+			out = {16'h0000,a};
+		end else begin //signed
+		//else	
+			if (a[15]==1) begin
+				out = {16'hFFFF,a};
+			end else begin
+				out = a;
+			end
+		end
+	end
+endmodule
