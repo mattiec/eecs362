bf0f9e21b70b55bb8e12524d8d1ad8c4bf3d46ae
Shantaviae Wynn <shantaviaewynn2014@u.northwestern.edu>
2014-02-18 22:42:41 -0600

    in progress

--- tests/single_cycle_tb.v
+++ tests/single_cycle_tb.v
@@ -22,28 +22,35 @@ initial begin
 
   //Load IMEM from file
    if (!$value$plusargs("instrfile=%s", filename)) begin
-      filename = "instr_unsignedsum.hex";
+      filename = "instr_fib.hex";
    end
    $readmemh(filename, top.instructionfetch.instrMemory.mem);
   // Load DMEM from file
   if (!$value$plusargs("datafile=%s", filename)) begin
-      filename = "data_unsignedsum.hex";
+      filename = "data_fib.hex";
   end
   $readmemh(filename, top.datamem.mem);
 
 //   $monitor("Instruction = %h  PC = %h\n\tIFU\n\t\tTarget = %h\n\t\ttakeBranch = %b\n\t\tjType = %b\n\t\treset = %b\n\t\tregJump = %b\n\t\tlinkTarget = %h\n\tDECODER\n\t\trs1 = %d\n\t\trs2 = %d\n\t\trd = %d\n\t\timmediate = %h\n\t\talu_signals = %b\n\t\tmem_size = %b\n\t\tmem_write = %b\n\t\tmem_ext = %b\n\t\treg_write = %b\n\t\tbranch_instr = %b\n\t\tjump_instr = %b\n\t\treg_data = %b\n\t\tI_type = %b\n\t\tbne = %b\n\t\tbeq = %b\n\t\tlink = %b\n\tREGISTERS\n\t\treg_data_in = %h\n\t\twrite_en = %b\n\t\taddr1 = %d\n\t\taddr2 = %d\n\t\twraddr = %d\n\t\tregA = %h\n\t\tregB = %h\n\tALU\n\t\tA = (HEX)%h   (DEC)%d\n\t\tB = (HEX)%h    (DEC)%d\n\t\talu_controls = %b\n\t\tzero = %b\n\t\talu_out = (HEX)%h    (DEC)%d\n\tDATA_MEMORY\n\t\taddr = %h\n\t\twData = %h\n\t\twriteEnable = %b\n\t\tdsize = %b\n\t\tmem_ext = %b\n\t\tdata_mem_out = %h\n\n",top.instruction,top.instructionfetch.PCout,top.instruction,top.Branch,top.jump_instruction,top.rst,top.Jump,top.instruction,top.RS,top.RT,top.RD,top.immed,top.ALUCtr,top.tmp,top.MemWr,top.tmp,top.RegWr,top.branch_instruction,top.jump_instruction,top.busWr,top.tmp,top.Branch_NotEqual,top.Branch,top.tmp,top.busWr,top.RegWr,top.RS,top.RT,top.WrAddr,top.busA,top.busB,top.busA,top.busA,top.bIN,top.bIN,top.ALUCtr,top.Zero,top.ALUOut,top.ALUOut,top.ALUOut,top.busB,top.MemWr,top.tmp,top.tmp,top.MemOut);
 
- $monitor("Clock = %b Instruction = %h PC = %h target=%h rs=%d rt=%d rd = %d busA=%d busB=%d bIN=%d ALUSrc=%b ALUOut=(hex)%h (dec)%d ALUCtr=%b \n MemOut=%h busWr=%h WrAddr=%d RegWr=%b Mem2Reg=%b Branch=%b Jump=%b Branch_instruction = %h Zero =%b \n WrEn = %b WrData=%h reset=%b regFile[6]=%h\n", top.clk,top.instruction, top.instructionfetch.PCout, top.instructionfetch.PCin, top.RS, top.RT, top.RD, top.busA, top.busB, top.bIN, top.ALUSrc, top.ALUOut, top.ALUOut, top.ALUCtr, top.MemOut, top.busWr, top.WrAddr, top.RegWr, top.Mem2Reg, top.instructionfetch.andBranch, top.Jump, top.branch_instruction, top.Zero, top.rFile.WrEn, top.rFile.WrData, reset, top.rFile.reg_file[6]); 
+$monitor("Clock = %b; Instruction = %h PC = %h target=%h rs=%d rt=%d rd = %d busA=%d busB=%d bIN=%d ALUSrc=%b ALUOut=(hex)%h (dec)%d ALUCtr=%b \n MemOut=%h busWr=%h WrAddr=%d RegWr=%b Mem2Reg=%b Branch=%b Jump=%b Branch_instruction = %h Zero =%b \n regFile[1] = %h regFile[2]=%h regFile[3]=%h regFile[4]=%h WrEn = %b WrData=%h reset=%b MemWr=%b\n", top.clk, top.instruction, top.instructionfetch.PCout, top.instructionfetch.PCin, top.RS, top.RT, top.RD, top.busA, top.busB, top.bIN, top.ALUSrc, top.ALUOut, top.ALUOut, top.ALUCtr, top.MemOut, top.busWr, top.WrAddr, top.RegWr, top.Mem2Reg, top.instructionfetch.andBranch, top.Jump, top.branch_instruction, top.Zero, top.rFile.reg_file[1], top.rFile.reg_file[2], top.rFile.reg_file[3], top.rFile.reg_file[4], top.rFile.WrEn, top.rFile.WrData, reset, top.MemWr); 
 
    #0 clock=0; reset=0;
    #2 reset=1;
    #2 reset=0;
 
-   #1000 $finish;
+   #5000 $finish;
 end
 
 always @(top.instruction) begin
    //This checks for a trap 0x300 (signifying the end of the file)
-   if (top.instruction == 32'h44000300) $finish;
+   if (top.instruction == 32'h44000300) begin	
+   
+		//for (i = 0; i < top.datamem.SIZE; i = i+1)
+		//	$display("%h\n", top.datamem.mem[i]);
+			
+		$finish;
+	end
 end
+
 endmodule
