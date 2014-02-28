b74f1178c02871714d1ea8233f8e840710d51f4a
Shantaviae Wynn <shantaviaewynn2014@u.northwestern.edu>
2014-02-27 20:11:11 -0600

    Optimized Register File

--- tests/regFile_test.v
+++ tests/regFile_test.v
@@ -8,19 +8,17 @@ module regFile_test;
 	wire [31:0] RData1;
 	wire [31:0] RData2;
 	
-	reg ftpt_write = 0;
-	reg ftpt_read = 0;
+	reg ftpt_write = 1;
+	reg ftpt_read = 1;
 	
 	reg WrEn;
 	reg rst;
 	reg clk;
 	
-	//regFile test(rst, clk, WrAddr, WrEn, WData, Read1, Read2, RData1, RData2);
-
-	regFile test(rst, clk, ftpt_write, ftpt_read, WrAddr, WrEn, WData, Read1, Read2, RData1, RData2);
-	//regFile test(rst, clk, WrAddr, WrEn, Read1, Read2, WData, RData1, RData2);
 	
+	regFile2 test(rst, clk, ftpt_write, ftpt_read, WrAddr, WrEn, WData, Read1, Read2, RData1, RData2);
 	
+	//register test(RData1, WData, WrEn, rst, clk);
 	
 	initial begin
 		rst = 1;
@@ -32,8 +30,9 @@ module regFile_test;
 	always #10 clk = ~clk;
 	
 	initial begin
-		$monitor("rst=%b clk=%b WrEn=%b WrAddr=%d WrData=%h RAddr1=%d RData1=%h RAddr2=%d RData2=%h", rst, clk, WrEn, WrAddr, WData, Read1, RData1, Read2, RData2);
-		
+		$monitor("rst=%b clk=%b ftpt_write=%b WrEn=%b WrAddr=%d WrData=%h RAddr1=%d RData1=%h RAddr2=%d RData2=%h", rst, clk, ftpt_write, WrEn, WrAddr, WData, Read1, RData1, Read2, RData2);
+		//$monitor("WrEn=%b WrAddr=%d WrData=%h RAddr1=%d RData1=%h RAddr2=%d RData2=%h O00=%h D00=%b D01=%b ",WrEn, WrAddr, WData, Read1, RData1, Read2, RData2, test.O00, test.D00, test.D01);
+		//$monitor("rst=%b clk=%b WrEn=%b WrData=%h RData1=%h", rst, clk, WrEn, WData, RData1);
 		Read1 = 0;
 		Read2 = 0;
 		WrEn = 0;
@@ -54,11 +53,12 @@ module regFile_test;
 		WData = 32'h8b004e;
 		@(posedge clk);
 		WrAddr = 5'd3;
-		WData = 323'hfde546;
+		WData = 32'hfde546;
 		
 		//stop writing data into register file
 		@(posedge clk);
 		WrEn = 0;
+		WData = 32'h0;
 		@(posedge clk);
 		
 		//reading data from register file
