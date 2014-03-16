4d8de64b5c880aba969e7d848a5b996bea209842
Shantaviae Wynn <shantaviaewynn2014@u.northwestern.edu>
2014-03-16 11:11:10 -0500

    updates to testbench

--- pipeline/tests/pp_tb.v
+++ pipeline/tests/pp_tb.v
@@ -32,12 +32,9 @@ initial begin
   end
   $readmemh(filename, top.datamem.mem);
 
-//$monitor("Rst = %b Clk = %b; Instruct = %h PC = %h imm=%h target=%h PCplus4 =%h Branch_PC =%h PCSrc =%b rs =%d rt =%d rd = %d \nJump = %b Branch = %b compareEq= %b PCSrc1 = %b PCSrc2 = %b PCSrc = %b RData1 = %h RData2 = %h\n", top.rst, top.clk, top.instruction, top.PCout, top.immed, top.PCin, top.PCplus4, top.Branch_PC, top.PCSrc, top.RS, top.RT, top.RD, top.Jump, top.Branch, top.compareEq, top.PCSrc1, top.PCSrc2, top.PCSrc, top.RData1, top.RData2); 
-//$monitor("iFetch: Clk=%b instruction = %h PCout = %h PCin = %h PCplus4 = %h Branch_PC = %h PCSrc = %h/n",top.clk, top.Instruction, top.PCout, top.PCin, top.PCplus4, top.Branch_PC, top.PCSrc);
-//$monitor("Decoder: instruction = %h RegDst = %b RegWr = %b RegFp_write = %b ALUCtr = %b ExtOp = %b ALUSrc = %b MemWr = %b Mem2Reg = %b Branch = %b Branch_NotEqual = %b Jump = %b\n", top.instruction, top.RegDst, top.RegWr, top.RegFp_write, top.ALUCtr, top.ExtOp, top.ALUSrc, top.MemWr, top.Mem2Reg, top.Branch, top.Branch_NotEqual, top.Jump);
-//$monitor("Exec: Clock = %b; Instruction = %h PC = %h target=%h rs=%d rt=%d rd = %d imm=%h Exec_immed = %h \nbusB = %h Exec_ALUSrc = %b ALUinB = %h busA = %h ALUout = %h WrAddr =%h", top.clk, top.instruction, top.PCout, top.PCin, top.RS, top.RT, top.RD, top.immed, top.Exec_immed, top.busB, top.Exec_ALUSrc, top.ALUinB, top.busA, top.ALUout, top.WrAddr);
-//$monitor("Mem: Addr = %h RData = %h WrData = %h WrEn = %b", top.Mem_Addr, top.Mem_RData, top.Mem_WrData, top.Mem_MemWr);
-$monitor("Clock = %b; Instruction = %h PC = %h imm=%h target=%h rs=%d rt=%d rd = %d busA = %h busB = %h WrAddr = %h WB_RegWr = %h busWr = %h\nregister[1] = %h register[2] = %h register[3] = %h register[4] = %h RD2 = %h RD3 = %h RD4 = %h", top.clk, top.instruction, top.PCout, top.immed, top.PCin, top.RS, top.RT, top.RD, top.RData1, top.RData2, top.WrAddr, top.WB_RegWr, top.busWr, top.rFile.ireg01.out, top.rFile.ireg02.out, top.rFile.ireg03.out, top.rFile.ireg04.out, top.RD2, top.RD3, top.RD4); 
+
+
+$monitor("PC: Clock = %b; Instruction = %h PC = %h target = %h PCplus4 = %h Branch_PC = %h PCSrc = %h\nDC: WrAddr = %h WB_RegWr = %h busWr = %h RS = %h RData1 = %h RT = %h RData2 = %h immed = %h\nEX: Exec_ALUctr = %h ALUout = %h Zero = %h busA = %h busB = %h RT2 = %h RD2 = %h Exec_RegDst = %h\nMEM: Mem_Addr = %h Mem_RData = %h Mem_WrData = %h Mem_MemWr = %h\nWB: WB_RData = %h WB_Addr = %h WB_Mem2Reg = %h busWr = %h\n", top.clk, top.instruction, top.PCout, top.PCin, top.PCplus4, top.Branch_PC, top.PCSrc, top.WrAddr, top.WB_RegWr, top.busWr, top.RS, top.RData1, top.RT, top.RData2, top.immed, top.Exec_ALUctr, top.ALUout, top.Zero, top.busA, top.busB, top.RT2, top.RD2, top.Exec_RegDst, top.Mem_Addr, top.Mem_RData, top.Mem_WrData, top.Mem_MemWr, top.WB_RData, top.WB_Addr, top.WB_Mem2Reg, top.busWr); 
 
 
 
@@ -48,7 +45,7 @@ $monitor("Clock = %b; Instruction = %h PC = %h imm=%h target=%h rs=%d rt=%d rd =
    #2 reset=1;
    #2 reset=0;
 
-   #60 $finish; //changed from 5000 to 60
+   #20 $finish; //changed from 5000 to 20
 end
 reg [0:31] instr;
 
